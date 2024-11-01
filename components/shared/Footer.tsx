559d0583c7d0efee4e6302d41039eafbb3cf6058
Prashantsha8586 <prashantsha8586@gmail.com>
2024-11-01 22:31:53 +0530

    last

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
     <footer className="border-t">
-      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row ">
-        <Link href="/">
-          <Image
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
             src="/assets/images/logo.svg"
             alt="logo"
             width={128}
@@ -18,7 +17,7 @@ const Footer = () => {
         <p>2023 Evently. All Rights reserved.</p>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
